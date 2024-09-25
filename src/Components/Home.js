61372b5d3589d14697c8119b195532c58ae94a86
Paul-Nathan <pngotte@gmail.com>
2024-09-25 12:19:28 +0100

    Blog preview Creation & Styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,27 @@
+import {useState} from 'react';
 
 
 
-export default function Home() {
+  const Home = () => {
+      const [blogs, setBlogs] = useState([
+          { title: 'First Blood', body: 'Adventure and Action Movie', author: 'John Rambo', id: 1},
+          { title: 'Second blood', body: 'Aventure and Action Movie', author: 'John Rambo', id:2},
+          { title: 'Last Blood', body: 'Adventure and Actor Movie', author: 'John Rambo', id: 3},
+          { title: 'Predator', body: 'Adventure, Actor Movie & Horror Movie', author: 'Arnold', id: 4},
+          { title: 'Comando', body: 'Adventure and Actor Movie', author: 'Arnold', id:5}
+      ])
+
   return (
-    <div className='home'>HomePage</div>
+    <div className='home'>
+
+        {blogs.map((blog)=>(
+            <div className='blog-preview' key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written By: {blog.author}</p>
+                <p>Type: {blog.body}</p>
+            </div>
+        ))}
+    </div>
   )
 }
+export default Home;
