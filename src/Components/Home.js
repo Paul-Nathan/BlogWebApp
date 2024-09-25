3d82483c3b071deb89aa9d01dd4a330097e2d9f1
Paul-Nathan <pngotte@gmail.com>
2024-09-25 13:32:02 +0100

    Using the filter & map method

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,6 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
+
 
 
 
@@ -13,14 +15,13 @@ import {useState} from 'react';
 
   return (
     <div className='home'>
-
-        {blogs.map((blog)=>(
-            <div className='blog-preview' key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written By: {blog.author}</p>
-                
-            </div>
-        ))}
+      <BlogList blogs = {blogs} title = "All Blogs" />
+       
+      <BlogList blogs ={blogs.filter((blog)=>{
+          if(blog.author === "John Rambo"){
+              return true
+            }
+      })} title="John Rambo's Blogs"/>
     </div>
   )
 }
