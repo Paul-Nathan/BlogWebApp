890da309453124366f76730eed62fa5495bc1a18
Paul-Nathan <pngotte@gmail.com>
2024-09-27 16:44:50 +0100

    Styling the homepage link in the NotFound.js

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
     <div className='not-found'>
         <h2>Sorry!!!!</h2>
-        <p>The page requested can't be found</p>
+        <p>The page requested can't be found</p><br></br>
+        <span>Please click here to return to home page: <Link to={`/`} style ={{color: "blue"}}>Home Page</Link></span>
     </div>
   )
 }
