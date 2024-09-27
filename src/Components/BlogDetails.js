e61b43a7c79571e620ebd4a04fc3bc1362bb5f3f
Paul-Nathan <pngotte@gmail.com>
2024-09-27 16:18:33 +0100

    inserting delete button

--- src/Components/BlogDetails.js
+++ src/Components/BlogDetails.js
@@ -2,12 +2,22 @@ import React from 'react'
 import {useParams} from 'react-router-dom'
 import useFetch from './useFetch'
 import loading from './Hourglass.gif'
+import { useHistory } from 'react-router-dom'
 
 
 function BlogDetails() {
     const {id} = useParams()
     const {data: blog, isPending, error} =
     useFetch('http://localhost:8000/blogs/' + id )
+    const history = useHistory()
+
+    //Delete Blog
+    const handleDelete = ()=>{
+        fetch("http://localhost:8000/blogs/" + blog.id, {
+            method: "DELETE"
+        })
+        history.push("/")
+    }
 
   return (
     <div className='blog-details'>
@@ -17,7 +27,8 @@ function BlogDetails() {
             <article>
                 <h2>{blog.title}</h2>
                 <div className='bodyDetails'>{blog.body}</div>
-                <h2>Acted  By: {blog.author}</h2>
+                <h2>Written By: {blog.author}</h2>
+                <button onClick={handleDelete}>Delete Blog</button>
             </article>
         )}
         </div>
