94b59ef8cbfcc2173424d531c0ff47da7c2f5bbe
Paul-Nathan <pngotte@gmail.com>
2024-09-27 16:22:07 +0100

    Updating the navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className='navbar'>
-        <h1>My Blog</h1>
+        <Link to ={`/`}><h1>My Blog</h1></Link>
         <div className='links'>
             <Link to="/">Home</Link>        
             <Link to="/create">New Blog</Link>        
