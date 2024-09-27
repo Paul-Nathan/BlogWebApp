0d4faebcbdaf30e0f9a4300829abe6d530aaa0f8
Paul-Nathan <pngotte@gmail.com>
2024-09-27 16:27:35 +0100

    NotFound.js created

---
+++ src/Components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='not-found'>
+        <h2>Sorry!!!!</h2>
+        <p>The page requested can't be found</p>
+    </div>
+  )
+}
+
+export default NotFound
