818dbae0c095317633d1d7304361ab3fb8a0939e
Paul-Nathan <pngotte@gmail.com>
2024-09-27 12:41:13 +0100

    Error message option updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,14 +18,21 @@ import loading from './Hourglass.gif'
         setTimeout(()=>{
           fetch(' http://localhost:8000/blogs')
         .then((res)=>{
-          
+          if(!res.ok){
+            throw Error("Could not fetch the data for that resource");
+          }
           return res.json()
         })
         .then((data)=>{
         console.log(data);
         setBlogs(data)
         setIsPending(false)
+        setError(null)
 
+        })
+        .catch((err)=>{
+          setIsPending(false)
+          setError(err.message)
         })
         }, 2000);
   
@@ -34,6 +41,7 @@ import loading from './Hourglass.gif'
 
   return (
     <div className='home'>
+      {error && <div>{error}</div> }
       {isPending && <div><img src={loading} alt='loading spinner'/></div>}
       {blogs &&<BlogList blogs = {blogs} title = "All Blogs" /> }    
     </div>
