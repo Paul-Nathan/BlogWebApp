94c80e0eb5d6cb60afe992eb3d74903da3c096af
Paul-Nathan <pngotte@gmail.com>
2024-09-27 12:51:40 +0100

    Seperation of concern by creating useFetch.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import useFetch from './useFetch';
 import BlogList from './BlogList';
 import loading from './Hourglass.gif'
 
@@ -6,38 +6,10 @@ import loading from './Hourglass.gif'
 
 
   const Home = () => {
-      const [blogs, setBlogs] = useState(null)
-      const[isPending, setIsPending] = useState(true)
-      const [error, setError] = useState(null)
 
-      setTimeout(()=>{
-
-      }, 2000);
-
-      useEffect(()=>{
-        setTimeout(()=>{
-          fetch(' http://localhost:8000/blogs')
-        .then((res)=>{
-          if(!res.ok){
-            throw Error("Could not fetch the data for that resource");
-          }
-          return res.json()
-        })
-        .then((data)=>{
-        console.log(data);
-        setBlogs(data)
-        setIsPending(false)
-        setError(null)
-
-        })
-        .catch((err)=>{
-          setIsPending(false)
-          setError(err.message)
-        })
-        }, 2000);
-  
-        
-      },[])
+    const{data: blogs, isPending, error} =
+    useFetch(' http://localhost:8000/blogs')
+      
 
   return (
     <div className='home'>
