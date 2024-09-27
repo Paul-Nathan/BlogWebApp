1403efd6115dad39f155dd5cc1a608df2a5da629
Paul-Nathan <pngotte@gmail.com>
2024-09-27 15:48:01 +0100

    Interface for new blog creation

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import {useState} from 'react';
 
-function Create() {
+const Create = ()=> {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [actor, setActor] = useState("");
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        const blog = {title, body, actor}
+
+        fetch('http://localhost:8000/blogs', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log("New Blog Added");
+        })
+    }
+
   return (
-    <div>Create</div>
+    <div className='create'>
+        <h1>Add New Blog</h1>
+        <form onSubmit={handleSubmit}>
+            <label>Movie Title:</label>
+            <input
+                    type='text'
+                    required 
+                    value ={title}
+                    onChange={(e)=>setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e)=> setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Actor</label>
+                <select
+                    value={actor}
+                    onChange={(e)=>setActor(e.target.value)}
+                    >
+                        <option value="John Rambo">John Rambo</option>
+                        <option value= "Will Smith">Will Smith</option>
+                        <option value= "Arnold">Arnold</option>
+                    </select>
+                    <button>Add Blog</button>
+        </form>
+        </div>
   )
 }
 
